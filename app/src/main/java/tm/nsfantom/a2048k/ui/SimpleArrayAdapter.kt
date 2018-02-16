7a4cdd41228c7878cd3c1b03273e1bd5f685083f
nsfantom <nick@tatarenkov.name>
2018-02-16 11:18:58 +0200

    fix ordered statistics

--- app/src/main/java/tm/nsfantom/a2048k/ui/SimpleArrayAdapter.kt
+++ app/src/main/java/tm/nsfantom/a2048k/ui/SimpleArrayAdapter.kt
@@ -1,6 +1,7 @@
 package tm.nsfantom.a2048k.ui
 
 import android.content.Context
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,14 +9,13 @@ import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.item_stats.view.*
 import tm.nsfantom.a2048k.R
 import tm.nsfantom.a2048k.model.StatItem
-import java.util.*
-import android.text.format.DateUtils
 import java.text.SimpleDateFormat
+import java.util.*
 
 
-class SimpleArrayAdapter(internal var context: Context, internal var statItems: ArrayList<StatItem>) : ArrayAdapter<StatItem>(context, -1, statItems) {
+class SimpleArrayAdapter(internal var context: Context, private var statItems: ArrayList<StatItem>) : ArrayAdapter<StatItem>(context, -1, statItems) {
 
-    fun updateData(statItems: ArrayList<StatItem>){
+    fun updateData(statItems: ArrayList<StatItem>) {
         this.statItems = statItems
         notifyDataSetChanged()
     }
@@ -24,17 +24,17 @@ class SimpleArrayAdapter(internal var context: Context, internal var statItems: 
         val inflater = parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val rowView = inflater.inflate(R.layout.item_stats, parent, false)
         val statItem = statItems[position]
-        rowView.tvResult.text = context.getString(R.string.score_label,statItem.result)
+        rowView.tvResult.text = context.getString(R.string.score_label, statItem.result)
         rowView.tvDate.text = formateDate(statItem.date)//statItem.date.toString()
         rowView.tvPosition.text = statItem.rank.toString()
         return rowView
     }
 
-    private fun formateDate(millis: Long):String{
-        val sdf = SimpleDateFormat("dd-MM-yyyy\nHH:mm")
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"))
+    private fun formateDate(millis: Long): String {
+        val sdf = SimpleDateFormat("dd-MM-yyyy\nHH:mm", Locale.ENGLISH)
+        sdf.timeZone = TimeZone.getTimeZone("GMT")
         val now = System.currentTimeMillis()
-        if ((now-millis)/1000 > 60*60*24*2)
+        if ((now - millis) / 1000 > 60 * 60 * 24 * 2)
             return sdf.format(Date(millis))
         return DateUtils.getRelativeTimeSpanString(millis, now, DateUtils.MINUTE_IN_MILLIS).toString()
     }
